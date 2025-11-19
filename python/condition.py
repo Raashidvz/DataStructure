bc645643a435e6fd48dc7707905f2c310872d10f
Raashidvz <raashidvz313@gmail.com>
2025-11-19 15:44:03 +0530

    Update condition.py

--- python/condition.py
+++ python/condition.py
@@ -1,10 +1,11 @@
 import re
 s=input()
 
-#r = re.fullmatch("(^if[\(|\s].+:$|^if[a-zA-Z0-9\_].*[a-zA-Z0-9\_)",s)
-#r = re.fullmatch("(^if[\s|\(].*:$|^if[\w]).+[\w]$",s)
 #r = re.fullmatch("(^if\s.*:$|^if\(.+\):$|^if\w.+\w$)",s)
-r = re.fullmatch("(^if\s.*:$|^if\(.+\):$|^if\w.+\w$|^for\s.+:$|for\w.+\w$|^while\s.+:$|^while\w.+\w$)",s)
+#r = re.fullmatch("(^if\s.*:$|^if\(.+\):$|^if\w.+\w$|^for\s.+:$|for\w.+\w$|^while\s.+:$|^while\w.+\w$)",s)
+r = re.fullmatch("(^if\s.*:$|^if\(.+\):$|^if[^\s\(].+[^:]$)",s)
+
+
 if r:
 	print("Valid")
 else:
