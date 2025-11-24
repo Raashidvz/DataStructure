712c3a06290102e0b1d47ac846ff3701be044af1
Raashidvz <raashidvz313@gmail.com>
2025-11-24 15:59:17 +0530

    Update and rename singlelinkedListsafe.py to ddl.py

---
+++ python/LinkedList/ddl.py
@@ -0,0 +1,81 @@
+import Node
+
+head=None
+tail=None
+def createNode(value):
+	n=Node.Node(value)
+	return n
+	
+def insert(head,node):
+	if head==None:
+		head=node
+		tail=node
+	else:
+		temp=head
+		while(temp.next!=None):
+			temp=temp.next
+		node.prev=temp	
+		temp.next=node
+		tail=node
+	return head,tail
+	
+def display(head):
+	if(head==None):
+		print("Empty List")
+		return
+	temp=head
+	while(temp!=None):
+		print(temp.data)
+		temp=temp.next
+	
+	print()
+	temp=tail
+	while(temp!=None):
+		print(temp.data)
+		temp=temp.prev
+	
+	
+def palindrome(head,tail):
+	h=head	
+	t=tail
+	s1=""
+	while(h!=None):
+		s1+=h.data
+		h=h.next
+	
+	s2=""
+	while(t!=None):
+		s2+=t.data
+		t=t.prev
+	
+	if(s1==s2):
+		print("The List is palindrome")
+	else:
+		print("List is not palindrome")
+	
+	
+	
+	
+	
+l1=createNode(1)
+l2=createNode(2)
+l3=createNode(3)
+l4=createNode(4)
+l5=createNode(5)
+
+
+head,tail=insert(head,l1)
+head,tail=insert(head,l2)
+head,tail=insert(head,l3)
+head,tail=insert(head,l4)
+head,tail=insert(head,l5)
+
+display(head)
+palindrome(head,tail)
+
+#l1=Node.Node(1)
+#l1=Node.Node(2)
+#l1=Node.Node(3)
+#l1=Node.Node(2)
+#l1=Node.Node(1)
+
